802af2da98f5b06ff895e8ff552a7413bcaf5781
m4nute <mgudinocontacto@gmail.com>
2023-05-09 20:21:34 -0300

    ✨ Display of date and read time at preview

--- components/Posts.tsx
+++ components/Posts.tsx
@@ -1,5 +1,6 @@
 import { allPosts } from "@/.contentlayer/generated"
 import Link from "next/link"
+import { parseISO, format } from "date-fns"
 
 export default function Posts() {
   return (
@@ -8,6 +9,9 @@ export default function Posts() {
         <article key={post._id}>
           <Link href={post.slug} className="no-underline">
             <h3 className="mb-0">{post.title}</h3>
+            <small>{post.readingTime.text}</small>
+            <br />
+            <small>{format(parseISO(post.date), "MMM dd, yyyy")}</small>
           </Link>
           {post.description && <p className="mt-0">{post.description}</p>}
         </article>
