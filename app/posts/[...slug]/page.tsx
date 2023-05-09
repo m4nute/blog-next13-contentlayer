554f0c5c3ba4f7d94ff4c4890d00ddc9b4a2a5e1
m4nute <mgudinocontacto@gmail.com>
2023-05-09 20:21:15 -0300

    ✨ Headings related configurations

--- app/posts/[...slug]/page.tsx
+++ app/posts/[...slug]/page.tsx
@@ -1,8 +1,8 @@
 import { notFound } from "next/navigation"
 import { allPosts } from "contentlayer/generated"
-
+import clsx from "clsx"
 import { Metadata } from "next"
-import { Mdx } from "@/components/mdx-components"
+import { Mdx } from "@/components/Template/mdx-components"
 
 interface PostProps {
   params: {
@@ -21,9 +21,7 @@ async function getPostFromParams(params: PostProps["params"]) {
   return post
 }
 
-export async function generateMetadata({
-  params,
-}: PostProps): Promise<Metadata> {
+export async function generateMetadata({ params }: PostProps): Promise<Metadata> {
   const post = await getPostFromParams(params)
 
   if (!post) {
@@ -32,13 +30,13 @@ export async function generateMetadata({
 
   return {
     title: post.title,
-    description: post.description,
+    description: post.description
   }
 }
 
 export async function generateStaticParams(): Promise<PostProps["params"][]> {
   return allPosts.map((post) => ({
-    slug: post.slugAsParams.split("/"),
+    slug: post.slugAsParams.split("/")
   }))
 }
 
@@ -50,13 +48,32 @@ export default async function PostPage({ params }: PostProps) {
   }
 
   return (
-    <article className="py-6 prose dark:prose-invert">
+    <article className="py-6 prose prose-sm dark:prose-invert">
       <h1 className="mb-2">{post.title}</h1>
-      {post.description && (
-        <p className="text-xl mt-0 text-slate-700 dark:text-slate-200">
-          {post.description}
-        </p>
-      )}
+      {post.description && <p className="mt-0 text-xl text-slate-700 dark:text-slate-200">{post.description}</p>}
+      {post.headings ? (
+        <div className="space-y-2 text-sm">
+          <div className="text-rose-100/30">Navigation Bar</div>
+
+          {post.headings.map((heading: any) => {
+            return (
+              <div key={heading.slug}>
+                <a
+                  href={`#${heading.slug}`}
+                  className={clsx(
+                    "block text-rose-100/50 underline-offset-2 transition-all hover:text-rose-100 hover:underline hover:decoration-rose-200/50",
+                    {
+                      "pl-2": heading.heading === 2,
+                      "pl-4": heading.heading === 3
+                    }
+                  )}>
+                  {heading.text}
+                </a>
+              </div>
+            )
+          })}
+        </div>
+      ) : null}
       <hr className="my-4" />
       <Mdx code={post.body.code} />
     </article>
