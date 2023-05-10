497d73f432fcec54d10999c5745d733835eed211
m4nute <mgudinocontacto@gmail.com>
2023-05-10 11:41:07 -0300

    ✨ Changed MDX component styles

--- components/Template/mdx-components.tsx
+++ components/Template/mdx-components.tsx
@@ -4,11 +4,11 @@ interface MdxProps {
   code: string
 }
 const components = {
-  h1: (props: any) => <h2 className=" pt-24 mt-3 text-xl font-medium border-t-2 border-[#2c2c2c] text-rose-100/90 sm:text-3xl" {...props} />,
-  h2: (props: any) => <h3 className="pt-9 mt-3 text-xl font-medium  sm:text-2xl" {...props} />,
+  h1: (props: any) => <h2 className=" pt-8 mt-6 text-xl font-medium border-t-2 border-[#2c2c2c] sm:text-3xl" {...props} />,
+  h2: (props: any) => <h3 className="pt-8 mt-3 text-xl font-medium sm:text-2xl text-[#e1e1e1]" {...props} />,
   h3: (props: any) => <h4 className="text-xl font-medium text-rose-100/90" {...props} />,
   h4: (props: any) => <h5 className="text-lg font-medium text-rose-100/90" {...props} />,
-  hr: (props: any) => <hr className="relative pt-9 border-t-2 border-rose-200/5 sm:pt-10" {...props} />,
+  hr: (props: any) => <hr className="relative my-5 border-t-2 border-rose-200/5 sm:pt-10" {...props} />,
   ul: (props: any) => (
     <ul
       className="space-y-3 [li>&]:mt-3 [&>li]:pl-7 before:[&>li]:absolute before:[&>li]:left-1 before:[&>li]:w-1.5 before:[&>li]:rounded-full before:[&>li]:bg-rose-100/20"
