69d7473167e1f646c5b872d171af223276143afc
m4nute <mgudinocontacto@gmail.com>
2023-05-10 23:39:17 -0300

    💄 Removed Radix avatar and replaced for local img

--- components/Navbar/Avatar.tsx
+++ components/Navbar/Avatar.tsx
@@ -1,15 +1,11 @@
 "use client"
 
-import * as Av from "@radix-ui/react-avatar"
-
+import Image from "next/image"
+import ProfilePic from "../../public/image.jpg"
 export default function Avatar() {
   return (
-    <Av.Root className="inline-flex w-12 h-12 rounded-full border border-gray-600">
-      <Av.Image
-        className="h-full w-full rounded-[inherit] object-cover"
-        src="https://media.licdn.com/dms/image/D4D03AQEZOpUSCHdyvQ/profile-displayphoto-shrink_800_800/0/1676219063905?e=1689206400&v=beta&t=SDPAE-nrf4aNLolIp9lNPRz3j2rJ4CyjcGn-rf5KBt4"
-        alt="Manuel Gudiño"
-      />
-    </Av.Root>
+    <div className="inline-flex relative w-12 h-12 rounded-full border border-gray-600">
+      <Image className="h-full w-full rounded-[inherit] object-cover" src={ProfilePic} alt="Manuel Gudiño" fill />
+    </div>
   )
 }
