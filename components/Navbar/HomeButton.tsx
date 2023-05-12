3f4ccfd3ee2815483424706507951258ef27e1a8
m4nute <mgudinocontacto@gmail.com>
2023-05-12 14:21:31 -0300

    🔥 Replaced framer motion with scale-105

--- components/Navbar/HomeButton.tsx
+++ components/Navbar/HomeButton.tsx
@@ -1,20 +1,14 @@
 "use client"
 
 import { IconHome } from "@tabler/icons-react"
-import { motion } from "framer-motion"
-import { useRouter } from "next/router"
 import Link from "next/link"
 
 export default function HomeButton() {
   return (
     <Link href="/">
-      <motion.button
-        className="bg-[#373737] p-2 rounded-lg transition-all"
-        whileHover={{ scale: 1.1 }}
-        whileTap={{ scale: 0.95 }}
-        transition={{ duration: 0.1 }}>
+      <button className="bg-[#373737] p-2 rounded-lg transition-all hover:scale-105 duration-150">
         <IconHome size={24} stroke={1.3} />
-      </motion.button>
+      </button>
     </Link>
   )
 }
