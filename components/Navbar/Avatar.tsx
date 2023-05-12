749e1fc0671b2172e7df719a068c4e70e853d344
m4nute <mgudinocontacto@gmail.com>
2023-05-12 15:19:24 -0300

    🐛 Next Image sizes to avoid warning

--- components/Navbar/Avatar.tsx
+++ components/Navbar/Avatar.tsx
@@ -5,7 +5,13 @@ import ProfilePic from "../../public/image.jpg"
 export default function Avatar() {
   return (
     <div className="inline-flex relative w-12 h-12 rounded-full border border-gray-600">
-      <Image className="h-full w-full rounded-[inherit] object-cover" src={ProfilePic} alt="Manuel Gudiño" fill />
+      <Image
+        className="h-full w-full rounded-[inherit] object-cover"
+        src={ProfilePic}
+        alt="Manuel Gudiño"
+        fill
+        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+      />
     </div>
   )
 }
--- components/PostPreview/Posts.tsx
+++ components/PostPreview/Posts.tsx
@@ -5,7 +5,7 @@ import PostTags from "./PostTags"
 
 export default function Posts() {
   return (
-    <div className="flex flex-col gap-6">
+    <div className="flex flex-col gap-4">
       {allPosts.map((post) => (
         <section key={post._id} className="p-5 hover:bg-[#171717] rounded-lg transition-all">
           <Link href={post.slug} className="no-underline">
