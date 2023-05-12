debaa96220c1e9de3eb5738068637177cedf9670
m4nute <mgudinocontacto@gmail.com>
2023-05-12 10:59:44 -0300

    💄 Added margin bottom to posts list

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Posts from "@/components/PostPreview/Posts"
 
 export default function Home() {
   return (
-    <div className="prose dark:prose-invert max-w-[680px] mx-auto">
+    <div className="prose dark:prose-invert max-w-[680px] mx-auto mb-14">
       <h2 className="mb-2">My Blog 📔</h2>
       <p className="mb-10">
         Welcome to my personal blog. Here, I&apos;ll be sharing my thoughts on various topics that I&apos;m passionate about. I also want to document
